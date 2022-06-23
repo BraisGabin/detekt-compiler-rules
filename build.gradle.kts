39968243d56eac1d7ff1abb2cc060de69ad3160b
Brais Gabín <braisgabin@gmail.com>
2022-06-23 10:41:36 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    `maven-publish`
+}
+
+group = "org.example.detekt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.20.0")
+
+    testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.20.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_class")
+    systemProperty("compile-snippet-tests", project.hasProperty("compile-test-snippets"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
